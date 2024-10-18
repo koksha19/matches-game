db835dd4a6cfc1de9dfbf8e58d3c0b16680fbffd
Lev <lev.bereza@gmail.com>
2024-10-18 18:45:18 +0300

    Select who will be the first to make a move

--- script.js
+++ script.js
@@ -6,11 +6,45 @@ let computerMatches = 0;
 
 const playerScore = document.getElementById("playerMatches");
 const computerScore = document.getElementById("computerMatches");
+const mainField = document.querySelector(".matches");
 const pile = document.getElementById("pile");
 const move = document.getElementById("move");
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+
+const startGame = () => {
+    const youButton = createButton("You", "Your move!");
+    const computerButton = createButton("Computer", "Wait for opponent!");
+    pile.style.width = "0";
+
+    youButton.addEventListener("click", () => {
+        computerButton.remove();
+    });
+    computerButton.addEventListener("click", () => {
+        youButton.remove();
+        setTimeout(computerMove, Math.random() * 10000);
+    });
+
+    move.textContent = 'Select the first to move';
+
+    mainField.appendChild(youButton);
+    mainField.appendChild(computerButton);
+}
+
+const createButton = (buttonText, labelText) => {
+    const button = document.createElement("button");
+    button.classList.add("button");
+    button.innerText = buttonText;
+    button.style.alignSelf = "center";
+    button.addEventListener("click", () => {
+        move.textContent = labelText;
+        updateState();
+        button.remove();
+        pile.style.width = "80%";
+    });
+    return button;
+}
 
 const updateState = () => {
     pile.textContent = '🔥'.repeat(matches);
@@ -18,7 +52,6 @@ const updateState = () => {
         const winner = playerMatches % 2 === 0 ? 'YOU' : 'COMPUTER';
         pile.textContent = `GAME OVER! ${winner} WON!`;
     }
-
 };
 
 const makeMove = (count) => {
@@ -30,7 +63,7 @@ const makeMove = (count) => {
     updateState();
 
     if (matches > 0) {
-        setTimeout(computerMove, 1000)
+        setTimeout(computerMove, Math.random() * 10000);
     }
 };
 
@@ -60,5 +93,5 @@ const clickButtons = () => {
     });
 }
 
-updateState();
+startGame()
 clickButtons();
