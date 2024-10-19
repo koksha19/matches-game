b891467038c8a0a74be1a7eeaa34f364cd91430c
Lev <lev.bereza@gmail.com>
2024-10-19 11:31:04 +0300

    Play again after the game is over

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const computerScore = document.getElementById("computerMatches");
 const mainField = document.querySelector(".matches");
 const pile = document.getElementById("pile");
 const move = document.getElementById("move");
-const buttons = document.querySelectorAll(".buttons .button");
+const buttons = document.querySelector(".buttons");
 
 const adjustParameters = () => {
-    const inputs = document.querySelector(".inputs")
+    const inputs = document.querySelector(".inputs");
     const nInput = document.getElementById("n");
     const mInput = document.getElementById("m");
     const submitBtn = document.getElementById("submit");
@@ -41,7 +41,7 @@ const determineFirstMove = () => {
     computerButton.addEventListener("click", () => {
         youButton.remove();
         createButtons();
-        setTimeout(computerMove, Math.random() * 10000);
+        setTimeout(computerMove, Math.random() * 1000);
     });
 
     move.textContent = 'Select the first to move';
@@ -55,13 +55,17 @@ const createButton = (buttonText, labelText, count) => {
     button.innerText = buttonText;
     button.style.alignSelf = "center";
     button.addEventListener("click", () => {
-        if (!count) {
+        if (labelText) {
             move.textContent = labelText;
             button.remove();
             updateState();
             pile.style.width = "80%";
-        } else {
+        } else if (count) {
             makeMove(count);
+        } else {
+            button.remove();
+            window.location.reload();
+            adjustParameters();
         }
     });
     return button;
@@ -77,19 +81,33 @@ const createButtons = () => {
 
 const updateState = () => {
     pile.textContent = '🔥'.repeat(matches);
-    let winner;
     if (matches === 0) {
-        if (playerMatches % 2 === 0) {
-            winner = "YOU";
-            pile.style.background = "#d27c28";
-        } else {
-            winner = "COMPUTER";
-            pile.style.background = "#ff0000";
-        }
-        pile.textContent = `GAME OVER! ${winner} WON!`;
-        pile.style.boxShadow = "5px 5px 5px #2d1546";
+        gameOver();
     }
 };
+
+const gameOver = () => {
+    const buttonsArray = Array.from(buttons.children);
+    let winner;
+
+    if (playerMatches % 2 === 0) {
+        winner = "YOU";
+        pile.style.background = "#d27c28";
+    } else {
+        winner = "COMPUTER";
+        pile.style.background = "#ff0000";
+    }
+    pile.textContent = `GAME OVER! ${winner} WON!`;
+    pile.style.boxShadow = "5px 5px 5px #2d1546";
+    move.textContent = "Wanna play again?";
+
+    for (const child of buttonsArray) {
+        console.log(child, buttons.children);
+        child.remove();
+    }
+    const positiveButton = createButton("Play again!", null, null);
+    buttons.appendChild(positiveButton);
+}
 
 const makeMove = (count) => {
     if (matches < count) return;
@@ -99,7 +117,7 @@ const makeMove = (count) => {
     move.innerText = 'Wait for opponent!';
     updateState();
     if (matches > 0) {
-        setTimeout(computerMove, Math.random() * 10000);
+        setTimeout(computerMove, Math.random() * 1000);
     }
 };
 
